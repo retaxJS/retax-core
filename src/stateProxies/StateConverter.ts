72ea6be1798ba16192573831f59011de9a18cb2c
Thomas Hourlier <thomas.hourlier@cnode.fr>
2016-06-02 19:07:33 +0200

    Add missing annotation on stateconverter since last inversify update.

--- src/stateProxies/StateConverter.ts
+++ src/stateProxies/StateConverter.ts
@@ -1,3 +1,4 @@
+import { injectable } from 'inversify';
 import { fromJS } from 'immutable';
 
 import { IStateConverter, IImmutableState } from './interfaces';
@@ -5,6 +6,7 @@ import { IStateConverter, IImmutableState } from './interfaces';
 /**
  * Feature inheritance. Not realy good practice...
  */
+@injectable()
 abstract class AStateConverter implements IStateConverter {
   public convertStateToImmutable<S extends IImmutableState>(object: Object = {}, nonImmutableKeys: string[] = []): S {
     const immutableObject: S = Object.keys(object).reduce((res: S, cur: string) => {
